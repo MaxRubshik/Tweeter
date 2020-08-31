cccfefcfddf4baf5c5426905ab83137a2a98c89c
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-08-31 12:39:40 +0300

    html -> mustache update

--- sweater/src/main/java/com/example/sweater/config/MvcConfig.java
+++ sweater/src/main/java/com/example/sweater/config/MvcConfig.java
@@ -9,9 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/").setViewName("home");
-//        registry.addViewController("/hello").setViewName("hello");
-//        registry.addViewController("/login").setViewName("login");
     }
 
 }
