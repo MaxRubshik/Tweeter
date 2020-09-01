e4fd75ba17742bb8afa75a755fc51dfddf529dd9
«MaxRubshik» <rubshikmax@gmail.com>
2020-09-01 17:36:20 +0300

    bug with port

--- sweater/src/main/java/com/example/sweater/controller/UserController.java
+++ sweater/src/main/java/com/example/sweater/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.sweater.controller;
 
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,12 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     @Autowired
     private UserRepo userRepo;
-    private Model mo;
 
     @GetMapping
     public String userList(Model model) {
         model.addAttribute("user", userRepo.findAll());
 
         return "userList";
+    }
+
+//    2. В модель добавляется атрибут с именем "users" и значением, которое
+//    вернет описанная функция (userRepo.findAll();) - всех пользователей в БД
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit.ftlh";
     }
 }
